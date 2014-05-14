04411c0100d37ac2bc21e01f39a99a8e3d539a83
fitzterra <github_subs@icave.net>
2014-05-14 11:31:34 +0200

    Initial commit with README

---
+++ README.mk
@@ -0,0 +1,6 @@
+Basic Key Server for public SSH keys
+====================================
+
+This project will be a basic keyserver to store and retrieve public SSH keys
+and manage them on a domain/host/user hierarchical basis.
+
